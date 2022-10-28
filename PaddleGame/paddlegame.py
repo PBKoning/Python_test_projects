a6e67f05d3abf6552e4e4a3ec89d3229a3f28b9f
PBKoning <pbkoning@gmail.com>
2022-10-28 21:36:58 +0200

    Added paddle movement by input of arrow keys

--- PaddleGame/paddlegame.py
+++ PaddleGame/paddlegame.py
@@ -4,10 +4,11 @@ import pygame
 
 # Constants
 SCREEN_WIDTH = 640
-SCREEN_HIGHT = 480
-SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HIGHT)
+SCREEN_HEIGHT = 480
+SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
 SURFACE_COLOR = (0, 0, 0)
 RED = RED = (255, 0, 0)
+PADDLE_SPEED = 5
 
 # Setup game
 pygame.init()
@@ -22,14 +23,28 @@ fps_clock = pygame.time.Clock()
 class Paddle(pygame.sprite.Sprite):
     def __init__(self, color, width, height):
         super().__init__()
-  
+        self.width = width
+        self.height = height
         self.image = pygame.Surface([width, height])
-        # self.image.fill(SURFACE_COLOR)
-        # self.image.set_colorkey(COLOR)
-  
+        
         pygame.draw.rect(self.image, color, pygame.Rect(0, 0, width, height))
   
         self.rect = self.image.get_rect()
+        self.rect.x = int((SCREEN_WIDTH - self.width) / 2) # position sprite in middle of screen
+        self.rect.y = SCREEN_HEIGHT - self.height - 5 # position sprite just above the bottom of the screen
+    
+    # function to move to the right
+    def moveRight(self, pixels):
+        self.rect.x += pixels
+        if self.rect.x > (SCREEN_WIDTH - self.width):
+            self.rect.x = SCREEN_WIDTH - self.width
+ 
+    # function to move to the left
+    def moveLeft(self, pixels):
+        self.rect.x -= pixels
+        if self.rect.x < 0:
+            self.rect.x = 0
+    
 
 # Create sprites and add to sprite list
 all_sprites_list = pygame.sprite.Group()
@@ -43,6 +58,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
+    # Handle input of keys
+    keys = pygame.key.get_pressed() 
+    if keys[pygame.K_LEFT]: # move left
+        player_paddle.moveLeft(PADDLE_SPEED)
+    if keys[pygame.K_RIGHT]: # move right
+        player_paddle.moveRight(PADDLE_SPEED)
+    if keys[pygame.K_q]: # quit game   
+        running = False
+
     # Draw game
     all_sprites_list.update()
     screen.fill(SURFACE_COLOR)
