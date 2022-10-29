048b39f5ab55c5d12cd1f2c418c564bbaba3b2ed
PBKoning <pbkoning@gmail.com>
2022-10-29 20:56:22 +0200

    Fullscreen on Chromebook added

---
+++ PygameFullScreenOnChromebook/fullscreen_chromebook.py
@@ -0,0 +1,23 @@
+# Run Pygame fullscreen on a Chromebook
+# Width and height need to match screen resolutiob to prevent an error
+
+import pygame
+
+pygame.display.init()
+
+width, height = pygame.display.Info().current_w, pygame.display.Info().current_h
+window_surface = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
+
+running = True
+clock = pygame.time.Clock()
+
+
+while running:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            running = False
+
+    pygame.display.update()
